0c32c7e426a1720d24295b36cc156dc0dada0d6c
Ivan Ustyantsev <vangold915@gmail.com>
2023-01-23 19:51:54 +0700

    Completed task 2

--- homework1/task2.java
+++ homework1/task2.java
@@ -1,5 +1,17 @@
 // Задание 2.
 // Вывести все простые числа от 1 до 1000
 public class task2 {
-    
+    public static void main(String[] args) {
+        System.out.println("Простые числа от 1 до 1000: ");
+        for(int i = 2; i < 1000; ++i){
+            int count = 0;
+            for(int j = 2; j <=i && count < 2;++j){
+              if(i % j == 0){
+                ++count;
+              }
+            }
+            if(count < 2)
+               System.out.println(i);
+          }
+    }
 }
