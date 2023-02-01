3e69447babd7307760d83bf9835c72e0185eed3b
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-01 20:01:29 +0700

    Initial commit homework3

---
+++ homework3/task1.java
@@ -0,0 +1,10 @@
+package homework3;
+
+// Задание 1.
+// Создать список целых чисел (заполнить случайными числами), удалить из списка четные числа.
+
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ homework3/task3.java
@@ -0,0 +1,10 @@
+package homework3;
+
+// Задание 3 *.
+// Реализовать алгоритм сортировки пузырьком
+
+public class task3 {
+    public static void main(String[] args) {
+        
+    }
+}
