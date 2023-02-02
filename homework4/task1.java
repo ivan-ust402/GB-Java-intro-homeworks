c6472459d73d3bd4409ce32a35037d8c90edc06b
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-02 14:23:17 +0700

    Initial commit for homework 4

---
+++ homework4/task1.java
@@ -0,0 +1,9 @@
+package homework4;
+// Задание 1.
+// Создать LinkedList целых чисел (заполнить случайными числами).
+// Реализуйте метод, который вернет “перевернутый” список.
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ homework4/task3.java
@@ -0,0 +1,8 @@
+package homework4;
+// Задание 3 *.
+// В калькулятор добавить возможность отменить последнюю операцию.
+public class task3 {
+    public static void main(String[] args) {
+        
+    }
+}
