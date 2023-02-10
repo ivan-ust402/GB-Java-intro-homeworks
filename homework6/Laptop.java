10329377e350eab7c52bf3573ab4445cbff1a486
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-10 21:02:29 +0700

    Completed base task

--- homework6/Laptop.java
+++ homework6/Laptop.java
@@ -1,5 +1,7 @@
 package homework6;
 
+import java.util.HashSet;
+
 public class Laptop {
     int id;
     String manufacturer;
@@ -42,7 +44,7 @@ public class Laptop {
         this.numberOfCores = numberOfCores;
         this.freq = freq;
         this.RAM = RAM;
-        this.kindOfMemory = kindOfMemory;
+        this.kindOfMemory = kindOfMemory.trim().toLowerCase();
         this.hardDisk = hardDisk;
         this.operatingSystem = operatingSystem.trim().toLowerCase();
         this.size = size;
@@ -52,13 +54,104 @@ public class Laptop {
     }
 
     public String toString() {
-        return "ID: " + id + ",\nПроизводитель: " + manufacturer  
-        +",\nМодель: " + model  + ",\nЦвет: " + color  + ",\nПроцессор: " 
-        + CPU + ",\nКоличество ядер: " + numberOfCores + ",\nЧастота: " 
-        + freq + ",\nRAM: " + RAM + ",\nВид памяти: " + kindOfMemory + 
-        ",\nHD: " + hardDisk  + ",\nСистема: " 
+        return "\n******* ID: " + id + ",\nПроизводитель: " 
+        + manufacturer +",\nМодель: " + model  + ",\nЦвет: " + color  
+        + ",\nПроцессор: " + CPU + ",\nКоличество ядер: " + numberOfCores +
+         ",\nЧастота: " + freq + ",\nRAM: " + RAM + ",\nВид памяти: " 
+        + kindOfMemory + ",\nHD: " + hardDisk  + ",\nСистема: " 
         + operatingSystem + ",\nРазмер экрана: " + size  + ",\nРазрешение: " 
         + screenResolution  + ",\nТехнология матрицы: " + matrixTechnology 
-        + ",\nЦена: " + price + "\n\n";
+        + ",\nЦена: " + price;
+    }
+
+    public static HashSet<Laptop> findLaptopByParam(HashSet<Laptop> inputSet, int key, String value) {
+        HashSet<Laptop> result = new HashSet<Laptop>();
+        if (key == 1) {
+            for (Laptop item : inputSet) {
+                if (item.manufacturer.equals(value)) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 2) {
+            for (Laptop item : inputSet) {
+                if (item.color.equals(value)) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 3) {
+            int cores;
+            if (value.isEmpty() || !value.matches("[-+]?\\d+")){
+                cores = -1;
+            } else {
+                cores = Integer.parseInt(value);
+            }
+            for (Laptop item : inputSet) {
+                if (item.numberOfCores == cores) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 4) {
+            int RAM;
+            if (value.isEmpty() || !value.matches("[-+]?\\d+")){
+                RAM = -1;
+            } else {
+                RAM = Integer.parseInt(value);
+            }
+            for (Laptop item : inputSet) {
+                if (item.RAM == RAM) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 5) {
+            for (Laptop item : inputSet) {
+                if (item.kindOfMemory.equals(value)) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 6) {
+            int HD;
+            if (value.isEmpty() || !value.matches("[-+]?\\d+")){
+                HD = -1;
+            } else {
+                HD = Integer.parseInt(value);
+            }
+            for (Laptop item : inputSet) {
+                if (item.hardDisk == HD) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 7) {
+            for (Laptop item : inputSet) {
+                if (item.operatingSystem.equals(value)) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 8) {
+            double size;
+            if (value.isEmpty() || !value.matches("[-+]?\\d+")){
+                size = -1;
+            } else {
+                size = Double.parseDouble(value);
+            }
+            for (Laptop item : inputSet) {
+                if (item.size == size) {
+                    result.add(item);
+                }
+            }
+        } else if (key == 9) {
+            int price;
+            if (value.isEmpty() || !value.matches("[-+]?\\d+")){
+                price = -1;
+            } else {
+                price = Integer.parseInt(value);
+            }
+            
+            for (Laptop item : inputSet) {
+                if (item.price == price) {
+                    result.add(item);
+                }
+            }
+        }
+        return result;
     }
 }
--- homework6/finalTask.java
+++ homework6/finalTask.java
@@ -1,7 +1,15 @@
 package homework6;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
+
 public class finalTask {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in, "Cp866"); 
+        HashSet<Laptop> laptopDB = new HashSet<>();
+
+        // Создаем экземпляры класса
         Laptop laptop0 = new Laptop(0, "DEXP", "Aquilon", "silver", "Intel Celeron N4020",  2, 1.1, 4, "SSD", 128, "Win10", 14, "1920X1080", "IPS", 15999);
         Laptop laptop1 = new Laptop(1, "Irbis", "NB257", "black", "Intel Celeron 3350", 2, 1.1, 4, "eMMC", 64, "Win10", 14, "1366X768", "IPS", 15999);
         Laptop laptop2 = new Laptop(2, "Digma", "EVE 14 C414", "black", "Intel Celeron N4020", 2, 1.1, 4, "eMMC", 64, "Win10", 14.1, "1920X1080", "IPS", 17999);
@@ -12,5 +20,137 @@ public class finalTask {
         Laptop laptop7 = new Laptop(7, "Asus", "Laptop 15 F515KA-BR110W", "silver", "Intel Celeron N4500", 2, 1.1, 4, "SSD", 128, "Win11", 15.6, "1366X768", "TN+film", 27999);
         Laptop laptop8 = new Laptop(8, "Apple", "MacBook Air", "silver", "Apple M1", 8, 3.2, 16, "SSD", 1000, "MacOS", 13.3, "2560X1600", "IPS", 151999);
         Laptop laptop9 = new Laptop(9, "Apple", "MacBook Pro 16", "silver", "Apple M2 Pro 12C", 12, 3.2, 16, "SSD", 512, "MacOS", 16, "3456X2234", "IPS", 259999);
+
+        laptopDB.add(laptop0);
+        laptopDB.add(laptop1);
+        laptopDB.add(laptop2);
+        laptopDB.add(laptop3);
+        laptopDB.add(laptop4);
+        laptopDB.add(laptop5);
+        laptopDB.add(laptop6);
+        laptopDB.add(laptop7);
+        laptopDB.add(laptop8);
+        laptopDB.add(laptop9);
+
+        try {
+            while (true) {
+                // Меню пользователя:
+                System.out.println("Добро пожаловать в каталог ноутбуков!");
+                System.out.println("Выберите действие:");
+                System.out.println("1 - Печать всех товаров");
+                System.out.println("2 - Поиск по каталогу");
+                System.out.println("0 - Выход");
+                int firstInput;
+                String fInput = scan.nextLine();
+                if (fInput.isEmpty() || !fInput.matches("[-+]?\\d+")){
+                    firstInput = 3;
+                } else {
+                    firstInput = Integer.parseInt(fInput);
+                }
+                if (firstInput == 1) {
+                    System.out.println(laptopDB);
+                } else if (firstInput == 2) {
+                    while (true) {
+                        System.out.println("Введите цифру, соответствующую критерию поиска:");
+                        System.out.println("1 - Производитель");
+                        System.out.println("2 - Цвет");
+                        System.out.println("3 - Количество ядер");
+                        System.out.println("4 - RAM");
+                        System.out.println("5 - Вид памяти");
+                        System.out.println("6 - Объем HD");
+                        System.out.println("7 - ОС");
+                        System.out.println("8 - Размер экрана");
+                        System.out.println("9 - Цена");
+                        System.out.println("0 - Выход в предыдущее меню");
+                        String sInput = scan.nextLine();
+                        int secondInput;
+                        if (sInput.isEmpty() || !sInput.matches("[-+]?\\d+")){
+                            secondInput = 10;
+                        } else {
+                            secondInput = Integer.parseInt(sInput);
+                        }
+                        
+                        String filter = new String();
+    
+                        switch(secondInput) {
+                            case 0: filter = "-1";
+                                    break;
+                            case 1: System.out.println("Введите производителя: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 2: System.out.println("Введите цвет: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 3: System.out.println("Введите кол-во ядер: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 4: System.out.println("Введите размер RAM: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 5: System.out.println("Введите вид HD: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 6: System.out.println("Введите объем HD: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 7: System.out.println("Введите ОС: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 8: System.out.println("Введите размер диагонали: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            case 9: System.out.println("Введите цену: ");
+                                    filter = scan.nextLine();
+                                    break;
+                            default: filter = "-2";
+    
+                        }
+    
+                        if (filter.equals("-1")) {
+                            System.out.println("Выходим в предыдущее меню...");
+                            break;
+                        } else if (filter.equals("-2")) {
+                            System.out.println("Вы ввели некорректное значение!");
+                            System.out.println("Попробуйте снова.");
+                            continue;
+                        } else {
+                            HashSet<Laptop> res = Laptop.findLaptopByParam(laptopDB, secondInput, filter.trim().toLowerCase());
+                            if (res.isEmpty()) {
+                                System.out.println("Ничего не найдено по вашему критерию!");
+                            } else {
+                                for (Laptop item : res) {
+                                    System.out.println("Производитель: " + 
+                                        item.manufacturer 
+                                        + ", Модель: " + item.model
+                                        + ", Цена: " + item.price);
+                                }
+                                
+                            }
+                        }
+
+                        System.out.println("Хотите попробовать снова? (да/нет)");
+                        String thirdInput = scan.nextLine().trim().toLowerCase();
+                        if (thirdInput.equals("нет")) {
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+
+                } else if (firstInput == 0) {
+                    System.out.println("Выход из программы...");
+                    break;
+                } else {
+                    System.out.println("Вы ввели невалидное значение!");
+                    System.out.println("Попробуйте снова.");
+                    continue;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            scan.close();
+        }
+
     }
 }
