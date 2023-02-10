ea91822047cc63b48a4eda8e57fd726978822f1e
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-10 15:25:54 +0700

    Initial task6 commit

--- homework5/task4.java
+++ homework5/task4.java
@@ -1,5 +1,7 @@
 package homework5;
 // На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
 public class task4 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
