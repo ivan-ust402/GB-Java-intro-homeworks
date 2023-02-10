de7630ddecc6faa17d5c2c1d5725658c0067b80b
Ivan Ustyantsev <vangold915@gmail.com>
2023-02-10 19:04:57 +0700

    Created class instances

--- homework6/Laptop.java
+++ homework6/Laptop.java
@@ -4,9 +4,12 @@ public class Laptop {
     int id;
     String manufacturer;
     String model;
+    String color;
     String CPU;
     int numberOfCores;
+    double freq;
     int RAM;
+    String kindOfMemory;
     int hardDisk;
     String operatingSystem;
     double size;
@@ -18,9 +21,12 @@ public class Laptop {
         int id,
         String manufacturer,
         String model,
+        String color,
         String CPU,
         int numberOfCores,
+        double freq,
         int RAM,
+        String kindOfMemory,
         int hardDisk,
         String operatingSystem,
         double size,
@@ -31,9 +37,12 @@ public class Laptop {
         this.id = id;
         this.manufacturer = manufacturer.trim().toLowerCase();
         this.model = model.trim().toLowerCase();
+        this.color = color;
         this.CPU = CPU.trim().toLowerCase();
         this.numberOfCores = numberOfCores;
+        this.freq = freq;
         this.RAM = RAM;
+        this.kindOfMemory = kindOfMemory;
         this.hardDisk = hardDisk;
         this.operatingSystem = operatingSystem.trim().toLowerCase();
         this.size = size;
@@ -44,10 +53,11 @@ public class Laptop {
 
     public String toString() {
         return "ID: " + id + ",\nПроизводитель: " + manufacturer  
-        +",\nМодель: " + model  + ",\nПроцессор: " + CPU  
-        + ",\nколичество ядер: " + numberOfCores  + ",\nRAM: " + RAM  
-        + ",\nHD: " + hardDisk  + ",\nСистема: " + operatingSystem  
-        + ",\nРазмер экрана: " + size  + ",\nРазрешение: " 
+        +",\nМодель: " + model  + ",\nЦвет: " + color  + ",\nПроцессор: " 
+        + CPU + ",\nКоличество ядер: " + numberOfCores + ",\nЧастота: " 
+        + freq + ",\nRAM: " + RAM + ",\nВид памяти: " + kindOfMemory + 
+        ",\nHD: " + hardDisk  + ",\nСистема: " 
+        + operatingSystem + ",\nРазмер экрана: " + size  + ",\nРазрешение: " 
         + screenResolution  + ",\nТехнология матрицы: " + matrixTechnology 
         + ",\nЦена: " + price + "\n\n";
     }
--- homework6/finalTask.java
+++ homework6/finalTask.java
@@ -1,5 +1,16 @@
 package homework6;
 
 public class finalTask {
-    
+    public static void main(String[] args) {
+        Laptop laptop0 = new Laptop(0, "DEXP", "Aquilon", "silver", "Intel Celeron N4020",  2, 1.1, 4, "SSD", 128, "Win10", 14, "1920X1080", "IPS", 15999);
+        Laptop laptop1 = new Laptop(1, "Irbis", "NB257", "black", "Intel Celeron 3350", 2, 1.1, 4, "eMMC", 64, "Win10", 14, "1366X768", "IPS", 15999);
+        Laptop laptop2 = new Laptop(2, "Digma", "EVE 14 C414", "black", "Intel Celeron N4020", 2, 1.1, 4, "eMMC", 64, "Win10", 14.1, "1920X1080", "IPS", 17999);
+        Laptop laptop3 = new Laptop(3, "DEXP", "Aquilon", "silver", "Intel Celeron N4020", 2, 1.1, 8, "SSD", 128, "Win11", 15.6, "1920X1080", "IPS", 18499);
+        Laptop laptop4 = new Laptop(4, "Asus", "Laptop 15 D543MA-DM1368", "white", "Intel Celeron N4020", 2, 1.1, 4, "HDD", 1000, "", 15.6, "1920X1080", "IPS", 21999);
+        Laptop laptop5 = new Laptop(5, "Acer", "Extensa 15 EX215-31-C3FF", "black", "Intel Celeron N4020", 2, 1.1, 4, "SSD", 128, "", 15.6, "1920X1080", "TN+film", 22999);
+        Laptop laptop6 = new Laptop(6, "HP", "255 G8", "silver", "AMD Athlon Gold 3150U", 2, 2.4, 4, "HDD", 1000, "", 15.6, "1366X768", "TN+film", 22999);
+        Laptop laptop7 = new Laptop(7, "Asus", "Laptop 15 F515KA-BR110W", "silver", "Intel Celeron N4500", 2, 1.1, 4, "SSD", 128, "Win11", 15.6, "1366X768", "TN+film", 27999);
+        Laptop laptop8 = new Laptop(8, "Apple", "MacBook Air", "silver", "Apple M1", 8, 3.2, 16, "SSD", 1000, "MacOS", 13.3, "2560X1600", "IPS", 151999);
+        Laptop laptop9 = new Laptop(9, "Apple", "MacBook Pro 16", "silver", "Apple M2 Pro 12C", 12, 3.2, 16, "SSD", 512, "MacOS", 16, "3456X2234", "IPS", 259999);
+    }
 }
